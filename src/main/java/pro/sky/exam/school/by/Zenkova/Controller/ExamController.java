50bdf5de23ebb6a2d539728bbad5964cf433c908
AnZenkova <nastyonka16@mail.ru>
2022-04-17 12:03:33 +1000

    Сделано

--- src/main/java/pro/sky/exam/school/by/Zenkova/Controller/ExamController.java
+++ src/main/java/pro/sky/exam/school/by/Zenkova/Controller/ExamController.java
@@ -5,18 +5,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.exam.school.by.Zenkova.Service.ExaminerService;
 
-@RestController
-@RequestMapping("/exam/java/exam")
-public class ExamController {
-
-    ExaminerService examinerService;
-
-    public ExamController(ExaminerService examinerService) {
-        this.examinerService = examinerService;
-    }
-
-    @RequestMapping("")
-    public String getQuestion(@RequestParam int amount) {
-        return String.valueOf(examinerService.getQuestions(amount));
-    }
-}
+//@RestController
+//@RequestMapping("/exam/java/exam")
+//public class ExamController {
+//
+//    ExaminerService examinerService;
+//
+//    public ExamController(ExaminerService examinerService) {
+//        this.examinerService = examinerService;
+//    }
+//
+//    @RequestMapping("")
+//    public String getQuestion(@RequestParam int amount) {
+//        return String.valueOf(examinerService.getQuestions(amount));
+//    }
+//}
--- src/main/java/pro/sky/exam/school/by/Zenkova/Controller/JavaQuestionController.java
+++ src/main/java/pro/sky/exam/school/by/Zenkova/Controller/JavaQuestionController.java
@@ -7,31 +7,31 @@ import org.springframework.web.bind.annotation.RestController;
 import pro.sky.exam.school.by.Zenkova.Service.QuestionService;
 import pro.sky.exam.school.by.Zenkova.data.Question;
 
-@RestController
-@RequestMapping("/exam/java")
-public class JavaQuestionController {
-
-    private QuestionService questionService;
-
-    public JavaQuestionController(QuestionService questionService) {
-        this.questionService = questionService;
-    }
-
-    @RequestMapping("/add")
-    public String addQuestion(@RequestParam String question,
-                              @RequestParam String answer) {
-        return questionService.add(question, answer) + " Добавлен";
-    }
-
-    @RequestMapping("/remove")
-    public String removeQuestion(@RequestParam String question,
-                                 @RequestParam String answer) {
-        Question questionNew = new Question(question, answer);
-        return questionService.remove(questionNew) + " Удален";
-    }
-
-    @RequestMapping("")
-    public String addQuestion() {
-        return String.valueOf(questionService.getAll());
-    }
-}
+//@RestController
+//@RequestMapping("/exam/java")
+//public class JavaQuestionController {
+//
+//    private QuestionService questionService;
+//
+//    public JavaQuestionController(QuestionService questionService) {
+//        this.questionService = questionService;
+//    }
+//
+//    @RequestMapping("/add")
+//    public String addQuestion(@RequestParam String question,
+//                              @RequestParam String answer) {
+//        return questionService.add(question, answer) + " Добавлен";
+//    }
+//
+//    @RequestMapping("/remove")
+//    public String removeQuestion(@RequestParam String question,
+//                                 @RequestParam String answer) {
+//        Question questionNew = new Question(question, answer);
+//        return questionService.remove(questionNew) + " Удален";
+//    }
+//
+//    @RequestMapping("")
+//    public String addQuestion() {
+//        return String.valueOf(questionService.getAll());
+//    }
+//}
