8d189513da417a9d6d9372cd3f80be9dbabc9c97
AnZenkova <nastyonka16@mail.ru>
2022-04-17 12:06:01 +1000

    Сделано

--- src/main/java/pro/sky/exam/school/by/Zenkova/Service/QuestionService.java
+++ src/main/java/pro/sky/exam/school/by/Zenkova/Service/QuestionService.java
@@ -6,13 +6,13 @@ import java.util.Collection;
 
 public interface QuestionService {
 
-//    Question add(String question, String answer);
-//
-//    Question add(Question question);
-//
-//    Question remove(Question question);
-//
-//    Collection<Question> getAll();
-//
-//    Question getRandomQuestion();
-//}
+    Question add(String question, String answer);
+
+    Question add(Question question);
+
+    Question remove(Question question);
+
+    Collection<Question> getAll();
+
+    Question getRandomQuestion();
+}
